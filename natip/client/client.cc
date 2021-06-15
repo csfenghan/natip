b0c10f8f97d589611b9b1dbe8ae5a1dd58294a56
fenghan <csfenghan@outlook.com>
2021-06-15 21:11:54 +0800

    修正了client的代码

--- natip/client/client.cc
+++ natip/client/client.cc
@@ -1,16 +1,18 @@
 #include <unix_api.h>
 
 const char server[] = "99809412876dcc63.natapp.cc";
-const short port = 13650;
+const char port[] = "13650";
 
 int main(int argc, char **argv) {
         int sockfd;
-	struct addrinfo *res;
+        struct addrinfo *res;
 
-	Getaddrinfo(server,NULL,NULL,&res);	
-	sockfd=Socket(res->ai_family,res->ai_socktype,res->ai_protocol);
-	Connect(sockfd,res->ai_addr,res->ai_addrlen);
+        Getaddrinfo(server, port, NULL, &res);
+        // do {
+        sockfd = Socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+        Connect(sockfd, res->ai_addr, res->ai_addrlen);
+        //} while ((res = res->ai_next) != NULL);
 
-	freeaddrinfo(res);
+        freeaddrinfo(res);
         exit(0);
 }
