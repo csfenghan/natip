b474c31ad3f4867ef3480f194a1ea0e481da8309
fenghan <csfenghan@outlook.com>
2021-06-15 20:50:21 +0800

    修改了客户端框架

--- natip/client/client.cc
+++ natip/client/client.cc
@@ -1,15 +1,16 @@
 #include <unix_api.h>
 
+const char server[] = "99809412876dcc63.natapp.cc";
+const short port = 13650;
+
 int main(int argc, char **argv) {
         int sockfd;
-        struct sockaddr_in server_addr;
+	struct addrinfo *res;
 
-        sockfd = Socket(AF_INET, SOCK_STREAM, 0);
+	Getaddrinfo(server,NULL,NULL,&res);	
+	sockfd=Socket(res->ai_family,res->ai_socktype,res->ai_protocol);
+	Connect(sockfd,res->ai_addr,res->ai_addrlen);
 
-        memset(&server_addr, 0, sizeof(server_addr));
-        server_addr.sin_port = htons(13650);
-        server_addr.sin_family = AF_INET;
-        Connect(sockfd, (SA *)&server_addr, sizeof(server_addr));
-
+	freeaddrinfo(res);
         exit(0);
 }
