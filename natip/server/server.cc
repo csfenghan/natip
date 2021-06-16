8ed9c25119c5cd7da287fc12ae969dd776a348ec
fenghan <csfenghan@outlook.com>
2021-06-16 20:04:29 +0800

    修改了server的bug

--- natip/server/server.cc
+++ natip/server/server.cc
@@ -1,35 +1,38 @@
 #include <unix_api.h>
 
 void printAddr(struct sockaddr_in addr) {
-        printf("addr:%s , port:%d\n",
-               inet_ntop(addr.sin_family, &addr, NULL, sizeof(struct sockaddr_in)),
-               ntohs(addr.sin_port));
+	char buf[1024];
+
+	inet_ntop(addr.sin_family, &addr, buf, sizeof(addr));
+	printf("addr:%s , port:%d\n",buf,
+			ntohs(addr.sin_port));
 }
 
 int main(int argc, char **argv) {
-        int listenfd, connfd;
-        pid_t child_pid;
-        socklen_t client_len;
-        struct sockaddr_in client_addr, server_addr;
+	int listenfd, connfd;
+	pid_t child_pid;
+	socklen_t client_len;
+	struct sockaddr_in client_addr, server_addr;
 
-        listenfd = Socket(AF_INET, SOCK_STREAM, 0);
+	listenfd = Socket(AF_INET, SOCK_STREAM, 0);
 
-        memset(&server_addr, 0, sizeof(server_addr));
-        server_addr.sin_family = AF_INET;
-        server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-        server_addr.sin_port = htons(8888);
-        Bind(listenfd, (SA *)&server_addr, sizeof(server_addr));
+	memset(&server_addr, 0, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	server_addr.sin_port = htons(8888);
+	Bind(listenfd, (SA *)&server_addr, sizeof(server_addr));
 
-        Listen(listenfd, 5);
+	Listen(listenfd, 5);
 
-        while (1) {
-                client_len = sizeof(client_addr);
-                connfd = Accept(listenfd, (SA *)&client_addr, &client_len);
-                if ((child_pid = Fork()) == 0) {
-                        Close(listenfd);
-                        printAddr(client_addr);
-                        exit(0);
-                }
-                Close(connfd);
-        }
+	while (1) {
+		client_len = sizeof(client_addr);
+		connfd = Accept(listenfd, (SA *)&client_addr, &client_len);
+		printf("accept successfully\n");
+		if ((child_pid = Fork()) == 0) {
+			Close(listenfd);
+			printAddr(client_addr);
+			exit(0);
+		}
+		Close(connfd);
+	}
 }
