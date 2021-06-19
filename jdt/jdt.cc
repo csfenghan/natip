c62e0eaf3c222510feed965e8cd9f93488d3abe3
fenghan <csfenghan@outlook.com>
2021-06-19 14:19:06 +0800

    正在添加解析功能

--- jdt/jdt.cc
+++ jdt/jdt.cc
@@ -10,6 +10,25 @@ namespace jdt {
 /***************************************
  *	一、JdtEncode实现
  **************************************/
+// 编码string数据
+std::pair<uint8_t *, uint32_t> Encode::encode(const std::string &data) {
+        std::pair<uint8_t *, uint32_t> result;
+        uint32_t len;
+        uint8_t *ptr;
+
+        // 设置协议内容
+        len = HEAD_SIZE + data.size();
+        result.first = new uint8_t[len];
+        result.second = len;
+        ptr = result.first;
+
+        encodeHead(ptr, SERVICE_SEND, TYPE_STRING, len);
+        ptr += HEAD_SIZE;
+        memcpy(ptr, data.data(), data.size());
+
+        return result;
+}
+
 // 编码jsoncpp数据
 #ifdef JSONCPP
 std::pair<uint8_t *, uint32_t> Encode::encode(const Json::Value &data) {
@@ -28,37 +47,78 @@ std::pair<uint8_t *, uint32_t> Encode::encode(const Json::Value &data) {
 
         // 2.设置头部字段（为了防止大小端不同，需要进行网络字节序转换）
         ptr = result.first;
-        *(uint8_t *)ptr = SERVICE_SEND;
-        ptr += 1;
-        *(uint8_t *)ptr = TYPE_JSON;
-        ptr += 1;
-        *(uint16_t *)ptr = 0;
-        ptr += 2;
-        *(uint32_t *)ptr = htonl(len);
+        encodeHead(ptr, SERVICE_SEND, TYPE_JSON, len);
 
         // 3.放置data数据
+        ptr += HEAD_SIZE;
         memcpy(ptr, body.data(), body.size());
 
         return result;
 }
 #endif
 
+// 释放分配的资源
 void Encode::release(const std::pair<uint8_t *, uint32_t> &data) { delete data.first; }
+
+// 按照struct MsgHead顺序设置协议头
+void Encode::encodeHead(uint8_t *ptr, int service, int type, int len) {
+        *(uint8_t *)ptr = service;
+        ptr += 1;
+        *(uint8_t *)ptr = type;
+        ptr += 1;
+        *(uint16_t *)ptr = 0;
+        ptr += 2;
+        *(uint32_t *)ptr = htonl(len);
+}
 
 /***************************************
  *	二、JdtDecode实现
  **************************************/
 
+// 初始化解析状态
+void Decode::init() { status_ = PARSING_INIT; }
+
+// 解析收到的数据流
 bool Decode::parse(uint8_t *data, uint32_t len) {
+        uint8_t *ptr;
+        uint32_t len_reserved;
+
+        // 保存数据
         for (uint32_t i = 0; i < len; i++)
                 data_parsing_.push_back(data[i]);
+
+        ptr = &data_parsing_[0];
+        len_reserved = data_parsed_.size();
+
+        // 解析len字节的数据
+        while (len_reserved > 0) {
+                if (status_ == PARSING_INIT || status_ == PARSING_HEAD) {
+                        if (!parseHead(ptr, len_reserved)) {
+                                return false;
+                        }
+                        ptr += HEAD_SIZE;
+                        len_reserved -= HEAD_SIZE;
+                        status_ = PARSING_BODY;
+                }
+                if (status_ == PARSING_BODY) {
+                }
+        }
 
         return true;
 }
 
+// 解析协议头
+// 如果解析失败，则返回false；否则返回true
 bool Decode::parseHead(uint8_t *data, uint32_t len) {
         if (len < HEAD_SIZE)
                 return false;
+        curr_head_.service = *(uint8_t *)data;
+        data += 1;
+        curr_head_.type = *(uint8_t *)data;
+        data += 1;
+        curr_head_.crc = *(uint16_t *)data;
+        data += 2;
+        curr_head_.len = ntohl(*(uint32_t *)data);
 
         return true;
 }
--- jdt/jdt.hpp
+++ jdt/jdt.hpp
@@ -24,7 +24,7 @@ namespace jdt {
  *	一、 消息体参数
  **************************************/
 // 1.协议头以及其用到的参数
-// (1) 魔数、版本号、头部大小
+// (1) 头部大小
 #define HEAD_SIZE 8 // 头部大小8字节
 
 // (2) 服务的类型
@@ -32,8 +32,9 @@ namespace jdt {
 #define SERVICE_REQ 2  //申请接收
 
 // (3) 数据的类型
-#define TYPE_JSON 1  // json数据
-#define TYPE_IMAGE 2 // image数据
+#define TYPE_JSON 1   // json数据
+#define TYPE_IMAGE 2  // image数据
+#define TYPE_STRING 3 // string数据
 
 // (3)协议头数据格式(实际上用不到，只是规范数据的格式，数据分别以magic、version、service、data_len顺序存放)
 struct MsgHead {
@@ -46,16 +47,23 @@ struct MsgHead {
 // 2.消息体（支持多种数据类型）
 class MsgBaseBody {
       public:
-        ~MsgBaseBody() {}
+        MsgBaseBody(int type) : type_(type) {}
+        virtual ~MsgBaseBody() {}
+
+        inline int getType() { return type_; }
+
+      private:
+        int type_;
 };
 template <typename T> class MsgBody : public MsgBaseBody {
       public:
-        MsgBody() {}
-        MsgBody(const T &data, int type) : data_(data), type_(type) {}
+        MsgBody(const int &type) : MsgBaseBody(type) {}
+        MsgBody(const T &data, const int &type) : MsgBaseBody(type), data_(data) {}
+
+        void setData(const T &data) { data_ = data; }
 
       private:
         T data_;
-        int type_;
 };
 
 /***************************************
@@ -63,18 +71,24 @@ template <typename T> class MsgBody : public MsgBaseBody {
  **************************************/
 class Encode {
       public:
-        // (1)jsoncpp文件传输（需要有jsoncpp库）
+        // 传输string字符串
+        std::pair<uint8_t *, uint32_t> encode(const std::string &data);
+
+        // jsoncpp文件传输（需要有jsoncpp库）
 #ifdef JSONCPP
         std::pair<uint8_t *, uint32_t> encode(const Json::Value &data);
 #endif
 
-        // (2)opencv image文件传输（需要有opencv库）
+        // opencv image文件传输（需要有opencv库）
 #ifdef OPENCV
         //       std::pair<uint8_t *, uint32_t> encode(const cv::Mat &img);
 
 #endif
-        // (3)销毁new的资源
+        // 销毁new的资源
         void release(const std::pair<uint8_t *, uint32_t> &data);
+
+      private:
+        void encodeHead(uint8_t *ptr, int service, int type, int len);
 };
 
 /***************************************
@@ -82,19 +96,27 @@ class Encode {
  **************************************/
 class Decode {
         enum ParserStatus {
-                PARSEING_INIT = 1, // 协议初始化
-                PARSEING_HEAD = 2, // 正在解析头部
-                PARSEING_BODY = 3, // 正在解析数据
+                PARSING_INIT = 1, // 协议初始化
+                PARSING_HEAD = 2, // 正在解析头部
+                PARSING_BODY = 3, // 正在解析数据
         };
 
       public:
+        // 初始化解析器状态
         void init();
+
+        // 解析接收到的字节流
         bool parse(uint8_t *data, uint32_t len);
 
       private:
-        std::deque<uint8_t> data_parsing_;
+        MsgHead curr_head_;                   // 正在解析的消息的消息头
+        std::deque<uint8_t> data_parsing_;    // 当前正在解析的字节流
+        std::deque<MsgBaseBody> data_parsed_; // 已经解析完的数据
+        ParserStatus status_;                 // 当前的解析状态
 
-        bool parseHead(uint8_t *data, uint32_t len);
+        // 解析协议头
+	// 如果解析失败，则返回false；否则返回true
+        bool parseHead(uint8_t *data,uint32_t len);
 };
 
 } // namespace jdt
