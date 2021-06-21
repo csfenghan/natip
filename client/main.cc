01e467481e57d59b758446c65e6eec9ef0349f1d
fenghan <csfenghan@foxmail.com>
2021-06-21 12:25:31 +0800

    修改了客户端程序，通过命令行指定配置文件

--- client/main.cc
+++ client/main.cc
@@ -4,11 +4,15 @@
 #include <string>
 
 int main(int argc, char **argv) {
+        if (argc != 2) {
+                fprintf(stderr, "usage: %s config file\n", argv[0]);
+                return -1;
+        }
         NatIpClient client;
         std::string data;
 
-        client.loadConfig("config.json");
+        client.loadConfig(std::string(argv[1]));
         client.tcpInit();
 
-	client.tcpClient();
+        client.tcpClient();
 }
